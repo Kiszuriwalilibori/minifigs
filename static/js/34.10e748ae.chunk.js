2a1f201de790824ddbb91fd9230e449fe94f7631
Piotr Maksymiuk <kiszuriwalilibori@poczta.onet.pl>
2022-10-31 23:09:17 +0100

    Updates

--- static/js/34.10e748ae.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkminifigs=self.webpackChunkminifigs||[]).push([[34],{4034:function(n,s,i){i.r(s),i.d(s,{default:function(){return f}});var e=i(9271),r=i(7825),u=i(184),c=function(){return(0,u.jsxs)("div",{className:"order",children:[(0,u.jsx)(r.PO,{}),(0,u.jsx)(r.ER,{})]})},f=(0,e.EN)(c)}}]);
-//# sourceMappingURL=34.10e748ae.chunk.js.map
